fed1bf347d7a89fa67bd56b08745d2e00753ed9e
ftahira1 <123195285+ftahira1@users.noreply.github.com>
2023-08-27 10:27:43 -0400

    updated main.jsx on client folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+import App from './App.jsx';
+import Home from './pages/Home';
+import Detail from './pages/SingleMeal';
+import NoMatch from './pages/NoMatch';
+import Login from './pages/LogIn';
+import Signup from './pages/SignUp';
+import OrderHistory from './pages/OrderHistory';
+import Success from './pages/Success';
+// import Maps from './pages/Maps';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NoMatch />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, 
+      // {
+      //   path: '/maps',
+      //   element: <Maps />
+      // },
+      {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }, {
+        path: '/orderHistory',
+        element: <OrderHistory />
+      }, {
+        path: '/products/:id',
+        element: <Detail />
+      }, {
+        path: '/success',
+        element: <Success />
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
