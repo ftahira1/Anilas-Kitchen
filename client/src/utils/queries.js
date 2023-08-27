ee74472e759bd7017f6130bb8bbf29c136d4d5c7
ftahira1 <123195285+ftahira1@users.noreply.github.com>
2023-08-27 12:14:51 -0400

    Upadted queries.js on client side

---
+++ client/src/utils/queries.js
@@ -0,0 +1,70 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_MEALS = gql`
+  query getMeals($menu: ID) {
+    meals(menu: $menu) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      menu {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($meals: [MealInput]) {
+    checkout(meals: $meals) {
+      session
+    }
+  }
+`;
+
+export const QUERY_ALL_MEALS = gql`
+  {
+    meals {
+      _id
+      name
+      description
+      price
+      quantity
+      menu {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_MENUES = gql`
+  {
+    menues {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        meals {
+          _id
+          name
+          description
+          price
+          quantity
+          image
+        }
+      }
+    }
+  }
+`;
