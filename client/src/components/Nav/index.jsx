51540591714dd67a96e3c56fdd03b52d5ba60952
ftahira1 <123195285+ftahira1@users.noreply.github.com>
2023-08-28 10:48:21 -0400

    Updated Nav on client side

---
+++ client/src/components/Nav/index.jsx
@@ -0,0 +1,67 @@
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
+
+function Nav() {
+
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/maps">
+              Find us!
+            </Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/orderStory">
+              Order Story
+            </Link>
+          </li>
+          <li className="mx-1">
+            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+            <a href="/" onClick={() => Auth.logout()}>
+              Logout
+            </a>
+          </li>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className="flex-row">
+        <li className="mx-1">
+            <Link to="/maps">
+              Find us!
+            </Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/signup">
+              SignUp!
+            </Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/login">
+              LogIn!
+            </Link>
+          </li>
+        </ul>
+      );
+    }
+  }
+
+  return (
+    <header className="flex-row px-1">
+      <h1>
+        <Link to="/">
+          <span role="img" aria-label="shopping bag">👨🏼‍🍳 </span>
+          Chef Albos
+        </Link>
+      </h1>
+
+      <nav>
+        {showNavigation()}
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
